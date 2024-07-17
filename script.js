0a8a644574059681fd3fcd1426570f7fb0b3d49e
abdusali1234 <abdusali1234@gmail.com>
2024-07-17 06:15:09 +0100

    added functionality to buttons, fixed issue with read status being null

--- script.js
+++ script.js
@@ -1,91 +1,104 @@
-function Book(title, author, nPages, readStatus){
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
-    this.nPages = nPages;
-    this.readStatus = readStatus;
+    this.pages = pages;
+    this.read = read;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, "reading");
-const the5AmClub = new Book('The 5 AM Club', 'Robin Sharma', 314, "not read yet");
-const nineteenEightyFour = new Book('Nineteen Eighty Four', 'George Orwell', 328, "finished");
+Book.prototype.toggleRead = function(){
+    this.read =!this.read;
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, true);
+const the5AmClub = new Book('The 5 AM Club', 'Robin Sharma', 314, false);
+const nineteenEightyFour = new Book('Nineteen Eighty Four', 'George Orwell', 328, true);
 
 const myLibrary = [theHobbit, the5AmClub, nineteenEightyFour];
 
-function createCard(book){
-    return `
-    <div class="card">
-        <h2>${book.title}</h2>
-        <h3>${book.author}</h3>
-        <h3>${book.nPages} ${book.nPages > 1 ? "pages" : "page"}</h3>
-        <h3>Read status: ${book.readStatus}</h3>
-    </div>
-    `
-}
+
+
+
 
 function displayBooks(){
     const card_container = document.querySelector(".card-container");
     card_container.innerHTML = "";
 
-    myLibrary.forEach(
-        (book) => {
-            card_container.innerHTML += createCard(book);
-        }
-    );
+    
+    myLibrary.forEach((book, index) => {
+        let cardEl = document.createElement("div");
+        cardEl.setAttribute("class", "card");
+        cardEl.setAttribute("id", index);
+        cardEl.innerHTML = `
+        <div class="side-banner"></div>
+        <div class="card-body">
+            <div class="card-header">
+                <h2>${book.title}</h2>
+                <h3>By ${book.author}</h3>
+            </div>
+            <div class="card-main">
+                <p class="pages">Pages: ${book.pages} ${book.pages > 1 ? "pages" : "page"}</p>
+                <p class="read-status">Read: ${book.read ? "Read" : "Not Read"}</p>
+                <button class="change-status" onclick="toggleRead(${index})">Mark as ${book.read ? " Not Read" : "Read"}</button>
+                <button class="remove-btn" onclick="removeBook(${index})">Remove</button>
+            </div>
+        </div>
+        `;
+        card_container.appendChild(cardEl);
+    });
 }
 
 function addBookstoLibrary(book){
     myLibrary.push(book);
 }
 
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
+function toggleRead(index){
+    myLibrary[index].toggleRead();
+    displayBooks();
+}
 
 
-const button = document.querySelector("#add-book");
+const newBtn = document.querySelector("#new-book");
 const dialog = document.querySelector("dialog");
 const addBook = document.querySelector("#add");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-const read = document.querySelector('input[name="read-status"]:checked');
+const title = document.querySelector("#title").value;
+const author = document.querySelector("#author").value;
+const pages = document.querySelector("#pages").value;
+const read = document.querySelector('#read').checked;
 
-const closeButton = document.querySelector("#close");
+const closeBtn = document.querySelector("#close");
 
 function clearForm(){
     title.value = "";
     author.value = "";
     pages.value = null;
-    Array.from( document.querySelectorAll('input[name="read-status"]:checked'), input => input.checked = false );
+    read.checked = false;
 }
 
-button.addEventListener("click", ()=>{
+newBtn.addEventListener("click", ()=>{
     dialog.showModal();
-    console.log(read);
-    console.log(read.value);
 });
 
 addBook.addEventListener("click", (event) => {
     event.preventDefault();
-    if (title.value !== "" && author.value !== "" && pages.value & read.value !== null){
-        addBookstoLibrary(new Book(title.value, author.value, pages.value, read.value));
-        console.log(read);
-        console.log(read.value);
+    if (title !== "" && author !== "" && pages){
+        addBookstoLibrary(new Book(title, author, pages, read));
         dialog.close();
         displayBooks();
         clearForm();
-        console.log(read);
-        console.log(read.value);
     }
 });
 
-closeButton.addEventListener("click", (event) =>{
+closeBtn.addEventListener("click", (event) =>{
     event.preventDefault();
     dialog.close();
-    console.log(read);
-    console.log(read.value);
     clearForm();
-    console.log(read);
-    console.log(read.value);
     
-})
+});
 
-clearForm();
 displayBooks();
+
